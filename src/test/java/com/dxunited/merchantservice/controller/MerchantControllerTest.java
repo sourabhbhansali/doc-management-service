7d5158cf388fa3035d93fbcde81d55947dcd25a6
sunil.jagtap <sunilgjagtap@gmail.com>
2021-11-18 23:53:56 +0530

    Create merchant service code changes

--- src/test/java/com/dxunited/merchantservice/controller/MerchantControllerTest.java
+++ src/test/java/com/dxunited/merchantservice/controller/MerchantControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
